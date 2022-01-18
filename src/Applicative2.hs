276021fee849a68fdd96ccd050385e789f9d4212
Kanezoh <asfaltmanta114@gmail.com>
2022-01-18 13:37:23 +0900

    add list context sample

--- src/Applicative2.hs
+++ src/Applicative2.hs
@@ -12,3 +12,31 @@ maybeInt = pure (6+)
 
 helloIO :: Maybe String
 helloIO = pure "Hello, World"
+
+-- 素数生成アルゴリズム
+primesToN :: Integer -> [Integer]
+primesToN n = filter isNotComposite twoThroughN
+  where twoThroughN = [2 .. n]
+        composite = pure (*) <*> twoThroughN <*> twoThroughN -- 合成数
+        isNotComposite = not . (`elem` composite)
+
+-- 大量のテストデータを作成する
+data User = User { name :: String 
+                 , gamerId :: Int
+                 , score :: Int } deriving Show
+
+testNames :: [String]
+testNames = [ "John"
+            , "Smith"
+            , "Paul"
+            , "Sara"]
+
+testIds :: [Int]
+testIds = [111,112,113]
+
+testScores :: [Int]
+testScores = [30221, 2141234, 142124,412414,412414]
+
+testUsers :: [User]
+testUsers = pure (User) <*> testNames <*> testIds <*> testScores
+
