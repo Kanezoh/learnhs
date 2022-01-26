ca6a0b9a18b269d54c2e74b9cfc87d7f0c77d3fa
Kanezoh <asfaltmanta114@gmail.com>
2022-01-26 14:34:17 +0900

    candidate funcs

--- src/Monad2.hs
+++ src/Monad2.hs
@@ -5,5 +5,50 @@ echo = do
   name <- getLine
   putStrLn name
 
+data Grade = F | D | C | B | A deriving (Eq, Ord, Enum, Show, Read)
+
+data Degree = HS | BA | MS | PhD deriving (Eq, Ord, Enum, Show, Read)
+
+data Candidate = Candidate { candidateId :: Int
+                           , codeReview :: Grade
+                           , cultureFit :: Grade
+                           , education :: Degree } deriving Show
+
+-- 候補者が要件を満たしているかどうか
+viable :: Candidate -> Bool
+viable candidate = all (== True) tests
+  where passedCoding = codeReview candidate > B
+        passedCultureFit = cultureFit candidate > C
+        educationMin = education candidate >= MS
+        tests = [passedCoding, passedCultureFit, educationMin]
+
+-- コマンドラインから候補者の情報を受け取るIOアクション
+readInt :: IO Int
+readInt = getLine >>= (return . read)
+
+-- readGrade = do
+--   grade <- getLine
+--   return $ read grade
+readGrade :: IO Grade
+readGrade = getLine >>= (return . read)
+
+readDegree :: IO Degree
+readDegree = getLine >>= (return . read)
+
+readCandidate :: IO Candidate
+readCandidate = do
+  putStrLn "enter id"
+  cId <- readInt
+  putStrLn "enter code grade"
+  codeGrade <- readGrade
+  putStrLn "enter culture fit grade"
+  cultureFitGrade <- readGrade
+  putStrLn "enter education"
+  degree <- readDegree
+  return (Candidate { candidateId = cId
+                    , codeReview = codeGrade
+                    , cultureFit = cultureFitGrade
+                    , education = degree})
+
 main :: IO()
 main = echo
